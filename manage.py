77b1b8ba71c34ca6b495652b68d52912c52e3a99
wangdengkai <wangdengkaiwy@163.com>
2018-07-28 08:54:42 +0800

    配置数据库,在manage.py中配置

--- manage.py
+++ manage.py
@@ -1,12 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+
+class Config(object):
+    '''项目的配置'''
+    DEBUG = True
+
+    #未数据库添加配置
+    SQLALCHEMY_DATABASE_URI="mysql://wangdengkai:wangdengkai@127.0.0.1:3306/infomation"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+db =SQLAlchemy(app)
+
 
 @app.route("/")
 def index():
     return "index"
 
 if __name__ == '__main__':
-    print(app.url_map)
-    app.run(debug=True)
+
+    app.run()
