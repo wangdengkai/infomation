7c16a18951d1980d09a0293d687ac7a8946ee39d
wangdengkai <wangdengkaiwy@163.com>
2018-07-28 10:15:38 +0800

    配置好session,将session保存到redis中,配置manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
-
+from flask_session import Session
 
 class Config(object):
     '''项目的配置'''
     DEBUG = True
+    SECRET_KEY ='ZqSFNHmDmg097SioiysE63xOgISjp0RcnTYM28DCx2kb4yGgX5Us6RPG1/jSZGCi'
+
 
     #未数据库添加配置
     SQLALCHEMY_DATABASE_URI="mysql://wangdengkai:wangdengkai@127.0.0.1:3306/infomation"
@@ -15,6 +17,20 @@ class Config(object):
     #Redis的配置
     REDIS_HOST ='127.0.0.1'
     REDIS_PORT =6379
+    #指定session保存的redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+
+    #Session保存位置
+    SESSION_TYPE='redis'
+    #是否开启session签名
+    SESSION_USE_SIGNER = True
+    #设置需要过期时间
+    SESSION_PERMANENT=False
+    #设置session,保存时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+
+
+
 
 app = Flask(__name__)
 
@@ -24,10 +40,17 @@ db =SQLAlchemy(app)
 #初始化redis,存储表单
 redis_store = StrictRedis(host=Config.REDIS_HOST,port =Config.REDIS_PORT)
 
-#开启当前项目CSRF保护
+#开启当前项目CSRF保护,制作服务器验证功能,cookie中的csrf_token和表单csrf_token需要我们自己实现..
 CSRFProtect(app)
+
+#设置session保存位置
+Session(app)
+
 @app.route("/")
 def index():
+    # session['name'] = 'laowang'
+    # redis_store.set("age","23")
+    print(session['name'])
     return "index"
 
 if __name__ == '__main__':
