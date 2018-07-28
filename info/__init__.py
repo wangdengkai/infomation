cab07d3d14b5226b43c1b0d573ed03b3a77a7b02
wangdengkai <wangdengkaiwy@163.com>
2018-07-28 11:27:34 +0800

    抽取app创建到函数中接受相关参数进行配置.

--- info/__init__.py
+++ info/__init__.py
@@ -9,17 +9,22 @@ from config import config
 '''
     info模块,是具体业务模块,实现相关业务.
 '''
+#初始化数据库,
+# 在flask很多拓展里面都可以先初始化托找的对象,然后调用init_appfangfa 初始化
+db = SQLAlchemy()
+def create_app(config_name):
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    #初始化db
+    db.init_app(app)
 
-app.config.from_object(config['develop'])
-db =SQLAlchemy(app)
+    #初始化redis,存储表单
+    redis_store = StrictRedis(host=config[config_name].REDIS_HOST,port =config[config_name].REDIS_PORT)
 
-#初始化redis,存储表单
-redis_store = StrictRedis(host=config['develop'].REDIS_HOST,port =config['develop'].REDIS_PORT)
+    #开启当前项目CSRF保护,制作服务器验证功能,cookie中的csrf_token和表单csrf_token需要我们自己实现..
+    CSRFProtect(app)
 
-#开启当前项目CSRF保护,制作服务器验证功能,cookie中的csrf_token和表单csrf_token需要我们自己实现..
-CSRFProtect(app)
-
-#设置session保存位置
-Session(app)
+    #设置session保存位置
+    Session(app)
+    return app
--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-
-from info import app,db
+from info import create_app
+from info import db
 '''
     这个模块专门提供启动程序的逻辑.
 '''
-
+#create_app  就类似于工厂方法
+app = create_app('development')
 #关联app
 manager = Manager(app)
 #将app于db关联
@@ -16,7 +17,7 @@ manager.add_command('db',MigrateCommand)
 
 @app.route("/")
 def index():
-    return "indexinsid"
+    return "indexi"
 
 if __name__ == '__main__':
     manager.run()
