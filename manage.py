4c836cd482b9be5ca18b74641b1d8228e5e182e7
wangdengkai <wangdengkaiwy@163.com>
2018-07-28 09:01:28 +0800

    配置redis数据库,在manage.py中配置

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 
 
 class Config(object):
@@ -10,11 +11,17 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI="mysql://wangdengkai:wangdengkai@127.0.0.1:3306/infomation"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+    #Redis的配置
+    REDIS_HOST ='127.0.0.1'
+    REDIS_PORT =6379
+
 app = Flask(__name__)
 
 app.config.from_object(Config)
 db =SQLAlchemy(app)
 
+#初始化redis,存储表单
+redis_store = StrictRedis(host=Config.REDIS_HOST,port =Config.REDIS_PORT)
 
 @app.route("/")
 def index():
