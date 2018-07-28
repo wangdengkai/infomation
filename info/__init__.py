d688f16aef46705d1212b2b609e466a84158a78c
wangdengkai <wangdengkaiwy@163.com>
2018-07-28 16:53:42 +0800

    基本设计框架抽取完成.

--- info/__init__.py
+++ info/__init__.py
@@ -51,7 +51,7 @@ def create_app(config_name):
     Session(app)
 
     #注册蓝图
-    from info.models.index import index_blu
+    from info.moduls.index import index_blu
     app.register_blueprint(index_blu)
 
     return app
