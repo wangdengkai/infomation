588ad9c536b8a4896ceed3947f9d41a85ecf7569
wangdengkai <wangdengkaiwy@163.com>
2018-07-30 09:23:33 +0800

    加载浏览器页面上方的小图标.favicon.ico

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,16 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from . import index_blu
-from info import redis_store
 
+
+#请求的首页
 @index_blu.route("/")
 def index():
 
     return render_template("news/index.html")
+
+
+#在打开网页的时候,浏览器会默认请求根路径下的favicon.ico
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
