20fcf0fbd9b3f384cab37b91f5b68e1aaac75532
wangdengkai <wangdengkaiwy@163.com>
2018-08-04 10:52:08 +0800

    增加命令行,创建超级用户,

--- manage.py
+++ manage.py
@@ -18,6 +18,29 @@ Migrate(app,db)
 #将迁移命令添加到manag er中
 manager.add_command('db',MigrateCommand)
 
+@manager.option('-n','-name',dest='name')
+@manager.option('-p','-password',dest='password')
+def createsuperuser(name,password):
+    '''创建管理员用户'''
+    if not all([name,password]):
+        print("参数不足")
+        return
+    from info.models import  User
+    user = User()
+    user.mobile = name
+    user.nick_name = name
+    user.password =password
+    user.is_admin = True
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+        print("创建成功")
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+
+
 
 
 
