995389df9874e22aee7b09e98d827998d89ee037
wangdengkai <wangdengkaiwy@163.com>
2018-08-04 16:35:04 +0800

    新闻办事编辑

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timedelta
 import time
 
-from flask import request, render_template, current_app, session, redirect, g, url_for
+from flask import request, render_template, current_app, session, redirect, g, url_for, jsonify
 
-from info import constants
+from info import constants, db
 from info.models import User, News
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 from . import admin_blu
 
 @admin_blu.route('/login',methods=['GET','POST'])
@@ -211,6 +212,125 @@ def news_review():
     }
 
     return render_template('admin/news_review.html',data=context)
+
+@admin_blu.route('/news_review_detail',methods=['GET','POST'])
+def news_review_detail():
+    '''新闻审核详情'''
+    if request.method == 'GET':
+    #获取新闻id
+        news_id=request.args.get('news_id')
+        if not news_id:
+            return render_template('admin/news_review_detail.html',data={'errmsg':'未查询到次新闻'})
+
+        #通过id查询新闻
+        news = None
+        try:
+            news = News.query.get(news_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+        if not news:
+            return render_template('admin/news_review_detail.html',data={'errmsg':'未查询到这个新闻'})
+
+        #返回数据
+        data = {'news':news.to_dict()}
+
+        return render_template('admin/news_review_detail.html',data=data)
+
+#     执行审核操作
+    #获取参数
+    news_id = request.json.get('news_id')
+    action = request.json.get('action')
+    #判断参数
+    if not all([news_id,action]):
+        return jsonify(errno=RET.PARAMERR,errmsg="参数错误")
+
+    if action not in ('accept','reject'):
+        return jsonify(errno=RET.PARAMERR,errmsg="参数错误")
+
+    news = None
+    try:
+        #查询新闻
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    if not news:
+        return jsonify(errno=RET.NODATA,errmsg="未查询到数据")
+
+    #根据不同的状态设置不同的值
+    if action == 'accept':
+        news.status = 0
+    else:
+        #拒绝通过,需要获取原因
+        reason = request.json.get('reason')
+        if not reason:
+            return jsonify(errno=RET.PARAMERR,errmsg="参数错误")
+
+        news.reason = reason
+        news.status = -1
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+
+        return jsonify(errno=RET.DBERR,errmsg="数据保存失败")
+
+
+    return jsonify(errno=RET.OK,errmsg="操作成功")
+
+@admin_blu.route('/news_edit')
+def news_edit():
+    '''返回新闻列表'''
+
+    page = request.args.get('p',1)
+    keywords = request.args.get('keywords','')
+
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    news_list =[]
+    current_page = 1
+    total_page = 1
+
+    try:
+        filters = [ ]
+        #如果有关键字
+        if keywords:
+            #添加关键爱你自搜索选项
+            filters.append(News.title.contains(keywords))
+
+        #查询
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page,constants.ADMIN_NEWS_PAGE_MAX_COUNT,False)
+
+        news_list = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+
+    except Exception as e:
+        current_app.logger.error(e)
+
+
+    news_dict_list = [ ]
+    for news in news_list:
+        news_dict_list.append(news.to_basic_dict())
+
+    context={'total_page':total_page,'current_page':current_page,'news_list':news_dict_list}
+
+    return render_template('admin/news_edit.html',data=context)
+
+
+
+
+
+
+
+
 
 
 
